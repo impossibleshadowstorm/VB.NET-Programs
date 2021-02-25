0b945d78a563003d3d0e76c0241c6ab78ad522fa
impossibleshadowstorm <67030264+impossibleshadowstorm@users.noreply.github.com>
2021-02-26 01:05:42 +0530

    Add files via upload

---
+++ Basic/CheckArmstrongNumber.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim num, temp, remainder As Integer
+        Dim cube As Double = 1
+        Dim arm As Integer = 0
+        Dim digits As Integer = 0
+
+        Console.WriteLine("                                     PROGRAM TO CHECK WHETHER THE NUMBER IS ARMSTRONG")
+        Console.WriteLine("                                  **--------------------------------------------------**")
+
+        Console.Write("ENTER A NUMBER:: ")
+        num = CInt(Console.ReadLine())
+        temp = num
+
+        While temp > 0
+            digits += 1
+            temp = Math.Floor(temp / 10)
+        End While
+
+        temp = num
+
+        While temp > 0
+            remainder = temp Mod 10
+            cube = remainder ^ digits
+            arm += cube
+            temp = Math.Floor(temp / 10)
+        End While
+
+        If arm = num Then
+            Console.WriteLine("\nYOUR ENTERED NUMBER IS AN ARMSTRONG NUMBER!!!")
+        Else
+            Console.WriteLine("\nYOUR ENTERED NUMBER IS NOT AN ARMSTRONG NUMBER!!!")
+        End If
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ Basic/ReverseNumber.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim num, remainder, reverse, test As Integer
+        Dim temp As Double
+
+        Console.WriteLine("ENTER THE NUMBER OF TESTCASES:: ")
+        test = CInt(Console.ReadLine())
+
+        For i As Integer = 1 To test
+
+            Console.Write("***********TESTCASE " & i)
+            Console.Write("***********")
+
+            reverse = 0
+
+            Console.WriteLine("" & vbNewLine)
+
+            Console.Write("ENTER A NUMBER:: ")
+            num = CInt(Console.ReadLine())
+
+            While num <> 0
+                remainder = num Mod 10
+                reverse = reverse * 10 + remainder
+                temp = num / 10
+                temp = Math.Floor(temp)
+                num = temp
+            End While
+
+            Console.WriteLine("REVERSED NUMBER IS:: " & reverse)
+            Console.WriteLine("" & vbNewLine)
+
+        Next
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
