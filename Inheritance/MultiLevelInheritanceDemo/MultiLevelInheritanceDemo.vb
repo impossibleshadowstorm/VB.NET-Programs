21d8b6f05c58c214635b72375a2386b0530d6b1e
Sumit Saurav <67030264+impossibleshadowstorm@users.noreply.github.com>
2021-04-10 03:36:09 +0530

    Add files via upload

---
+++ Inheritance/MultiLevelInheritanceDemo/MultiLevelInheritanceDemo.vb
@@ -0,0 +1,55 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim student As ComputerEngineeringStudent = New ComputerEngineeringStudent()
+
+        Console.WriteLine("" & vbNewLine & vbNewLine + "                                    PROGRAM TO SHOW DEMO OF SUB KEYWORD USING ADDITION PROGRAM")
+        Console.WriteLine("                                 **------------------------------------------------------------** " & vbNewLine)
+
+        student.personDetail()
+        student.studentDetail()
+        student.engineeringStudentDetail()
+        student.computerEngineeringStudentDetail()
+
+        Console.ReadKey()
+    End Sub
+
+    Public Class Person
+        Public Sub personDetail()
+            Console.WriteLine("" & vbNewLine & vbNewLine + "                                                    GREAT GRANDPARENT CLASS" & vbNewLine)
+            Console.WriteLine("                                                    -----------------------" & vbNewLine)
+
+            Console.WriteLine("EVERYONE IS A PERSON WHETHER HE/SHE IS A STUDENT, DOCTOR, ENGINEER, ...." & vbNewLine)
+        End Sub
+    End Class
+
+    Public Class Student : Inherits Person
+        Public Sub studentDetail()
+            Console.WriteLine("" & vbNewLine & vbNewLine + "                                                      GRANDPARENT CLASS" & vbNewLine)
+            Console.WriteLine("                                                      -----------------" & vbNewLine & vbNewLine)
+
+            Console.WriteLine("EVERYONE IS A STUDENT WHETHER HE/SHE IS STUDYING MEDICAL ENGINEERING, ...." & vbNewLine)
+        End Sub
+    End Class
+
+    Public Class EngineeringStudent : Inherits Student
+        Public Sub engineeringStudentDetail()
+            Console.WriteLine("" & vbNewLine & vbNewLine + "                                                        PARENT CLASS" & vbNewLine)
+            Console.WriteLine("                                                        ------------" & vbNewLine & vbNewLine)
+
+            Console.WriteLine("EVERYONE IS A ENGINEERING STUDENT WHETHER HE/SHE IS STUDYING FOR COMPUTER, MECHANICAL, BIOCHEMICAL FIELDS, ...." & vbNewLine)
+        End Sub
+    End Class
+
+    Public Class ComputerEngineeringStudent : Inherits EngineeringStudent
+        Public Sub computerEngineeringStudentDetail()
+            Console.WriteLine("" & vbNewLine & vbNewLine + "                                                         CHILD CLASS" & vbNewLine)
+            Console.WriteLine("                                                         -----------" & vbNewLine & vbNewLine)
+
+            Console.WriteLine("EVERYONE IS A COMPUTER ENGINEERING STUDENT WHETHER HE/SHE IS PERSUING BCA, Bsc, MCA, ...." & vbNewLine)
+        End Sub
+    End Class
+
+
+End Module
---
+++ Inheritance/SingleInheritance/SingleInheritance.vb
@@ -0,0 +1,43 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim FacultySalarySum As ScienceFaculty = New ScienceFaculty()
+
+        Console.WriteLine("" & vbNewLine & vbNewLine + "                                    PROGRAM TO SHOW DEMO OF SINGLE INHERITANCE USING FACULTY SALARY AND BONUS")
+        Console.WriteLine("                                 **---------------------------------------------------------------------------** " & vbNewLine & vbNewLine)
+
+        Console.WriteLine("NOTE:: NORMAL SALARY FOR ALL FACULTY IS 5K AND BONUSES ARE GIVEN ACCORDING TO DEPARTMENTS!!!" & vbNewLine & vbNewLine)
+
+        Console.WriteLine("ACCESSING PARENT CLASS METHOD THROUGH CHILD CLASS OBJECT...")
+        Console.WriteLine("EVERY FACULTY SALARY IS:: {0}", FacultySalarySum.EveryFacultySalary() & vbNewLine)
+
+        Console.WriteLine("ACCESSING BOTH CLASS METHODS THROUGH CHILD CLASS OBJECT...")
+        Console.WriteLine("EVERY FACULTY SALARY IS:: {0}", FacultySalarySum.EveryFacultySalary())
+        Console.WriteLine("SCIENCE FACULTY BONUS IS:: {0}", FacultySalarySum.scienceFacultyBonus())
+        Console.WriteLine("SO, THE TOTAL SALARY OF SCIENCE FACULTY ALONG WITH BONUS IS:: {0}", FacultySalarySum.EveryFacultySalary + FacultySalarySum.scienceFacultyBonus())
+
+        Console.ReadKey()
+    End Sub
+
+    Public Class Faculty
+
+        Public Function EveryFacultySalary() As Integer
+            Dim salary As Integer = 5000
+
+            Return salary
+        End Function
+
+    End Class
+
+    Public Class ScienceFaculty : Inherits Faculty
+
+        Public Function scienceFacultyBonus() As Integer
+            Dim bonus As Integer = 20000
+
+            Return bonus
+        End Function
+
+    End Class
+
+End Module
